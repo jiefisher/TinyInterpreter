69428c3088a41cb06351bce5e59794934d6a77d8
fangjie <fangjie@patsnap.com>
2022-06-08 19:15:32 +0800

    llvm

--- main.py
+++ main.py
@@ -32,15 +32,16 @@ class MyVisitor(LabeledExprVisitor):
     fnty = ir.FunctionType(ir.DoubleType(), ())
 
     # Create an empty module...
-    module = ir.Module(name=__file__)
-    # and declare a function named "fpadd" inside it
-    func = ir.Function(module, fnty, name="main")
-
-    # Now implement the function
-    block = func.append_basic_block(name="entry")
-    builder = ir.IRBuilder(block)
+    
     
     scope = Scope()
+    scope.module = ir.Module(name=__file__)
+    # and declare a function named "fpadd" inside it
+    scope.func = ir.Function(module, fnty, name="main")
+
+    # Now implement the function
+    scope.block = scope.func.append_basic_block(name="entry")
+    scope.builder = ir.IRBuilder(scope.block)
     output_var_name = None
     func_dic = {}
     func_param_table = []
@@ -51,6 +52,7 @@ class MyVisitor(LabeledExprVisitor):
         #     return value
         if key in self.scope.param:
             value = self.scope.param[key]
+            print(value,"param")
             return value
         elif len(self.func_param_table)>0:
             self.scope.tag = 1
@@ -74,18 +76,40 @@ class MyVisitor(LabeledExprVisitor):
                     self.scope = self.scope.post
             return value
         
-        return 0
+        return (0,ir.Constant(ir.DoubleType(), 0))
 
     def visitFuncdelcExpr(self, ctx):
         func_name = ctx.ID().getText()
         self.func_dic[func_name] = ctx 
         
-        fnty = ir.FunctionType(ir.DoubleType(), ())
-        func = ir.Function(self.module, fnty, name=func_name)
-
+        self.scope.post = Scope()
+        self.scope.post.prev = self.scope
+        self.scope = self.scope.post
+        
+        param_li = []
+        if ctx.idList():
+            for x in ctx.idList().ID():
+                param_li.append(ir.DoubleType())
+        self.scope.fnty = ir.FunctionType(ir.DoubleType(), param_li)
+        self.scope.func = ir.Function(self.module, self.scope.fnty, name=func_name)
+        for i in range(len(param_li)):
+            self.func_param_table.append((0,self.scope.func.args[i]))
         # Now implement the function
-        block = func.append_basic_block(name="entry")
-        builder = ir.IRBuilder(block)
+        self.scope.block = self.scope.func.append_basic_block(name="entry")
+        self.scope.builder = ir.IRBuilder(self.scope.block)
+        # self.block_index += 1
+        
+        
+        
+        
+        self.visit(ctx.block())
+        
+        
+        self.scope.param = {}
+        if self.scope.prev != None:
+            self.scope = self.scope.prev
+            
+        
         
         return 0
     
@@ -93,26 +117,29 @@ class MyVisitor(LabeledExprVisitor):
         func_id = str(ctx.ID().getText())
         print(ctx.getText())
         func_ctx = self.func_dic[func_id]
-        self.scope.post = Scope()
-        self.scope.post.prev = self.scope
-        self.scope = self.scope.post
-        # self.block_index += 1
-        
-        
+        func_params = []
         if ctx.exprList():
             for x in ctx.exprList().expr():
                 value = self.visit(x)
+                print(value,x.getText())
                 self.func_param_table.append(value)
+                func_params.append(value)
         
-        value = self.visit(func_ctx.block())
         
+        
+        # return self.builder.call(callee_func, call_args, 'calltmp')
+        
+        
+        print(self.func_param_table)
         
         self.scope.param = {}
         if self.scope.prev != None:
             self.scope = self.scope.prev
         
+        callee_func = self.module.get_global(func_id)
+        call_args = [x[1] for x in func_params]
         # self.func_dic.pop(func_id)
-        return value
+        return (0,self.scope.builder.call(callee_func, call_args, 'calltmp'))
     
     def visitFunctionCallExpr(self, ctx):
         value = self.visit(ctx.functionCall())
@@ -127,6 +154,8 @@ class MyVisitor(LabeledExprVisitor):
             self.visit(state)
         if ctx.expr():
             value = self.visit(ctx.expr())
+            print(value)
+            self.scope.builder.ret(value[1])
             return value
         return 0
     
@@ -142,8 +171,8 @@ class MyVisitor(LabeledExprVisitor):
             self.scope.param[l] = r
         self.output_var_name = l
         
-        rhs_val = self.builder.alloca(ir.DoubleType())
-        self.builder.store(r[1], rhs_val)
+        rhs_val = self.scope.builder.alloca(ir.DoubleType())
+        self.scope.builder.store(r[1], rhs_val)
         return (int(r[0]),rhs_val)
     
     def visitNumberExpr(self, ctx):
@@ -160,19 +189,19 @@ class MyVisitor(LabeledExprVisitor):
 
         op = ctx.op.text
         operation =  {
-        '+': lambda: (l[0] + r[0],self.builder.fadd(l[1], r[1], 'addtmp')),
-        '-': lambda: (l[0] - r[0],self.builder.fsub(l[1], r[1], 'subtmp')),
-        '*': lambda: (l[0] * r[0],self.builder.fmul(l[1], r[1], 'multmp')),
-        '/': lambda: (l[0] / r[0],self.builder.fdiv(l[1], r[1], 'divtmp')),
-        '^': lambda: (l[0] ^ r[0],self.builder.xor(l[1], r[1], 'xortmp')),
-        '==': lambda: (l[0] == r[0],self.builder.icmp_signed('==',l[1], r[1], 'equaltmp')),
-        '!=': lambda: (l[0] != r[0],self.builder.icmp_signed('!=',l[1], r[1], 'notequaltmp')),
-        '>': lambda: (l[0] > r[0],self.builder.icmp_signed('>',l[1], r[1], 'bigtmp')),
-        '<': lambda: (l[0] < r[0],self.builder.icmp_signed('<',l[1], r[1], 'smalltmp')),
+        '+': lambda: (l[0] + r[0],self.scope.builder.fadd(l[1], r[1], 'addtmp')),
+        '-': lambda: (l[0] - r[0],self.scope.builder.fsub(l[1], r[1], 'subtmp')),
+        '*': lambda: (l[0] * r[0],self.scope.builder.fmul(l[1], r[1], 'multmp')),
+        '/': lambda: (l[0] / r[0],self.scope.builder.fdiv(l[1], r[1], 'divtmp')),
+        '^': lambda: (l[0] ^ r[0],self.scope.builder.xor(l[1], r[1], 'xortmp')),
+        '==': lambda: (l[0] == r[0],self.scope.builder.icmp_signed('==',l[1], r[1], 'equaltmp')),
+        '!=': lambda: (l[0] != r[0],self.scope.builder.icmp_signed('!=',l[1], r[1], 'notequaltmp')),
+        '>': lambda: (l[0] > r[0],self.scope.builder.icmp_signed('>',l[1], r[1], 'bigtmp')),
+        '<': lambda: (l[0] < r[0],self.scope.builder.icmp_signed('<',l[1], r[1], 'smalltmp')),
         '>=': lambda: (l[0] >= r[0],self.buildericmp_signed('>=',l[1], r[1], 'bigequqaltmp')),
-        '<=': lambda: (l[0] <= r[0],self.builder.icmp_signed('<=',l[1], r[1], 'smallequaltmp')),
-        '&&': lambda: (l[0] and r[0],self.builder.and_(l[1], r[1], 'andtmp')),
-        '||': lambda: (l[0] or r[0],self.builder.or_(l[1], r[1], 'ortmp')),
+        '<=': lambda: (l[0] <= r[0],self.scope.builder.icmp_signed('<=',l[1], r[1], 'smallequaltmp')),
+        '&&': lambda: (l[0] and r[0],self.scope.builder.and_(l[1], r[1], 'andtmp')),
+        '||': lambda: (l[0] or r[0],self.scope.builder.or_(l[1], r[1], 'ortmp')),
         }
         return operation.get(op, lambda: None)()
     
