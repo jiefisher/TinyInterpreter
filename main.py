fe706ccc8162440a195d0387df45b8f950b67033
fangjie <fangjie@patsnap.com>
2022-06-13 19:13:55 +0800

    llvm cmp

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ from enum import Enum
 import llvmlite.ir as ir
 import llvmlite.binding as llvm
 
+llvm.initialize()
+llvm.initialize_native_target()
+llvm.initialize_native_asmprinter()  # yes, even this one
+
 def get_username():
     from pwd import getpwuid
     from os import getuid
@@ -36,6 +40,7 @@ class MyVisitor(LabeledExprVisitor):
     
     scope = Scope()
     scope.module = ir.Module(name=__file__)
+    
     # and declare a function named "fpadd" inside it
     scope.func = ir.Function(module, fnty, name="main")
 
@@ -151,13 +156,14 @@ class MyVisitor(LabeledExprVisitor):
     
     def visitBlock(self,ctx):
         for state in ctx.statement():
-            self.visit(state)
+            cc = self.visit(state)
+            print(state.getText(),"c")
         if ctx.expr():
             value = self.visit(ctx.expr())
-            print(value)
+            print(value,"block")
             self.scope.builder.ret(value[1])
             return value
-        return 0
+        return cc
     
     def visitAssignExpr(self, ctx):
         
@@ -168,11 +174,18 @@ class MyVisitor(LabeledExprVisitor):
             
             self.scope.param[l][ind] = r
         else:
-            self.scope.param[l] = r
+            if str(r[1].type) =="i1":
+                assign_r = self.scope.builder.uitofp(r[1], ir.DoubleType(), 'booltmp')
+            else:
+                assign_r = r[1]
+            rhs_val = self.scope.builder.alloca(assign_r.type)
+            
+            self.scope.builder.store(assign_r, rhs_val)
+            self.scope.param[l] = (r[0],assign_r)
         self.output_var_name = l
         
-        rhs_val = self.scope.builder.alloca(ir.DoubleType())
-        self.scope.builder.store(r[1], rhs_val)
+        
+        
         return (int(r[0]),rhs_val)
     
     def visitNumberExpr(self, ctx):
@@ -194,20 +207,21 @@ class MyVisitor(LabeledExprVisitor):
         '*': lambda: (l[0] * r[0],self.scope.builder.fmul(l[1], r[1], 'multmp')),
         '/': lambda: (l[0] / r[0],self.scope.builder.fdiv(l[1], r[1], 'divtmp')),
         '^': lambda: (l[0] ^ r[0],self.scope.builder.xor(l[1], r[1], 'xortmp')),
-        '==': lambda: (l[0] == r[0],self.scope.builder.icmp_signed('==',l[1], r[1], 'equaltmp')),
-        '!=': lambda: (l[0] != r[0],self.scope.builder.icmp_signed('!=',l[1], r[1], 'notequaltmp')),
-        '>': lambda: (l[0] > r[0],self.scope.builder.icmp_signed('>',l[1], r[1], 'bigtmp')),
-        '<': lambda: (l[0] < r[0],self.scope.builder.icmp_signed('<',l[1], r[1], 'smalltmp')),
-        '>=': lambda: (l[0] >= r[0],self.buildericmp_signed('>=',l[1], r[1], 'bigequqaltmp')),
-        '<=': lambda: (l[0] <= r[0],self.scope.builder.icmp_signed('<=',l[1], r[1], 'smallequaltmp')),
+        '==': lambda: (l[0] == r[0],self.scope.builder.fcmp_unordered('==',l[1], r[1], 'equaltmp')),
+        '!=': lambda: (l[0] != r[0],self.scope.builder.fcmp_unordered('!=',l[1], r[1], 'notequaltmp')),
+        '>': lambda: (l[0] > r[0],self.scope.builder.fcmp_unordered('>',l[1], r[1], 'bigtmp')),
+        '<': lambda: (l[0] < r[0],self.scope.builder.fcmp_unordered('<',l[1], r[1], 'smalltmp')),
+        '>=': lambda: (l[0] >= r[0],self.scope.builder.fcmp_unordered('>=',l[1], r[1], 'bigequqaltmp')),
+        '<=': lambda: (l[0] <= r[0],self.scope.builder.fcmp_unordered('<=',l[1], r[1], 'smallequaltmp')),
         '&&': lambda: (l[0] and r[0],self.scope.builder.and_(l[1], r[1], 'andtmp')),
         '||': lambda: (l[0] or r[0],self.scope.builder.or_(l[1], r[1], 'ortmp')),
         }
         return operation.get(op, lambda: None)()
     
     def visitIfStatementExpr(self, ctx):
+        print(self.visit(ctx.ifStat().expr()),"cc")
         if bool(self.visit(ctx.ifStat().expr())):
-            self.visit(ctx.ifStat().block())
+            print(self.visit(ctx.ifStat().block()),"dd")
         for x in ctx.elseIfStat():
             if bool(self.visit(x.expr())):
                 self.visit(x.block())
@@ -259,6 +273,35 @@ class MyVisitor(LabeledExprVisitor):
         return (f"{ctx.getText()} {get_username()}")
 
 
+def create_execution_engine():
+    """
+    Create an ExecutionEngine suitable for JIT code generation on
+    the host CPU.  The engine is reusable for an arbitrary number of
+    modules.
+    """
+    # Create a target machine representing the host
+    target =  llvm.Target.from_triple("i386-pc-linux-gnu")
+    target_machine = target.create_target_machine()
+    # And an execution engine with an empty backing module
+    backing_mod = llvm.parse_assembly("")
+    engine = llvm.create_mcjit_compiler(backing_mod, target_machine)
+    return engine
+
+
+def compile_ir(engine, llvm_ir):
+    """
+    Compile the LLVM IR string with the given engine.
+    The compiled module object is returned.
+    """
+    # Create a LLVM module object from the IR
+    mod = llvm.parse_assembly(llvm_ir)
+    mod.verify()
+    # Now add the module and make sure it is ready for execution
+    engine.add_module(mod)
+    engine.finalize_object()
+    return mod
+
+
 if __name__ == "__main__":
     file = open("newtest.gl","r")
 
@@ -278,4 +321,29 @@ if __name__ == "__main__":
     output = visitor.visit(tree)
 
     print(visitor.scope.param[visitor.output_var_name])
+    print(visitor.output_var_name)
+    
+    visitor.scope.builder.ret(visitor.scope.param[visitor.output_var_name][1])
     print(visitor.module)
+    llvm_ir = str(visitor.module)
+    engine = create_execution_engine()
+    mod = compile_ir(engine, llvm_ir)
+    
+    llvm_module = llvm.parse_assembly(llvm_ir)
+    tm = llvm.Target.from_default_triple().create_target_machine()
+
+    with llvm.create_mcjit_compiler(llvm_module, tm) as ee:
+        ee.finalize_object()
+        fptr = ee.get_function_address("main")
+        py_func = CFUNCTYPE(c_double)(fptr)
+        print(py_func())
+
+
+
+    target = llvm.Target.from_default_triple()
+    tm = target.create_target_machine(jit=False)
+    target_machine = tm
+
+    obj_bin = target_machine.emit_object(mod)
+    f = open("a.out","wb")
+    f.write(obj_bin)
